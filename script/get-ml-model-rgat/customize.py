a379d4319cb1e577a4d9dbe55d078243670a2e5c
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-12-21 09:15:29 +0000

    Fix rgat download path, added libbz2 deps for draw-graph-from-json

--- script/get-ml-model-rgat/customize.py
+++ script/get-ml-model-rgat/customize.py
@@ -21,7 +21,7 @@ def postprocess(i):
 
     if env.get('CM_ML_MODEL_RGAT_CHECKPOINT_PATH', '') == '':
         env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH'] = env.get(
-            'RGAT_CHECKPOINT_PATH', env['CM_ML_MODEL_PATH'])
+            'RGAT_CHECKPOINT_PATH', os.path.join(env['RGAT_DIR_PATH'], "RGAT.pt"))
 
     if env.get('CM_ML_MODEL_PATH', '') == '':
         env['CM_ML_MODEL_PATH'] = env['CM_ML_MODEL_RGAT_CHECKPOINT_PATH']
