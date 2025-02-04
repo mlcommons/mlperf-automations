4b53d0856f428b0b3d08de571d26ba43b6a335b9
Sujith Kanakkassery <82945126+sujik18@users.noreply.github.com>
2025-02-04 15:19:28 +0530

    Update customize.py

--- script/app-image-classification-onnx-py/customize.py
+++ script/app-image-classification-onnx-py/customize.py
@@ -3,12 +3,10 @@ import os
 import shutil
 import logging
 
-# Configure the logger
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 def preprocess(i):
-
+    
+    logger = i['automation'].logger
     os_info = i['os_info']
     env = i['env']
 
