9bd521e090fe5c4776740dc058e0e50cfbd5a704
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-02-12 04:48:12 +0530

    Added quotes for file paths in draw-graph-from-json-data

--- script/draw-graph-from-json-data/customize.py
+++ script/draw-graph-from-json-data/customize.py
@@ -14,13 +14,13 @@ def preprocess(i):
 
     quiet = (env.get('MLC_QUIET', False) == 'yes')
 
-    env['MLC_RUN_CMD'] = f"""{env['MLC_PYTHON_BIN_WITH_PATH']} {os.path.join(env['MLC_TMP_CURRENT_SCRIPT_PATH'],"process-mlc-deps.py")}  {env['MLC_JSON_INPUT_FILE']}"""
+    env['MLC_RUN_CMD'] = f"""{env['MLC_PYTHON_BIN_WITH_PATH']} "{os.path.join(env['MLC_TMP_CURRENT_SCRIPT_PATH'],"process-mlc-deps.py")}" "{env['MLC_JSON_INPUT_FILE']}" """
 
     if env.get('MLC_OUTPUT_IMAGE_PATH', '') != '':
-        env['MLC_RUN_CMD'] += f""" --output_image {env['MLC_OUTPUT_IMAGE_PATH']}"""
+        env['MLC_RUN_CMD'] += f""" --output_image "{env['MLC_OUTPUT_IMAGE_PATH']}" """
 
     if env.get('MLC_OUTPUT_MERMAID_PATH', '') != '':
-        env['MLC_RUN_CMD'] += f""" --output_mermaid {env['MLC_OUTPUT_MERMAID_PATH']}"""
+        env['MLC_RUN_CMD'] += f""" --output_mermaid "{env['MLC_OUTPUT_MERMAID_PATH']}" """
 
     return {'return': 0}
 
