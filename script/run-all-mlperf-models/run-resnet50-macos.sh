69aac503119b0b8563c34a1a9ac6fa01f2d99811
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-02-16 23:44:41 +0000

    Fix execution_mode in run-all script

--- script/run-all-mlperf-models/run-resnet50-macos.sh
+++ script/run-all-mlperf-models/run-resnet50-macos.sh
@@ -18,6 +18,7 @@ function run() {
   echo "$1"
   echo ""
   if [[ ${MLC_FAKE_RUN} != 'yes' ]]; then
+    exit 1
     eval "$1"
     exit_if_error
   fi
@@ -44,12 +45,12 @@ find_performance_cmd='mlcr generate-run-cmds,inference,_find-performance \
 submission_cmd='mlcr generate-run-cmds,inference,_submission,_all-scenarios \
 --model=$model --implementation=$implementation --device=$device --backend=$backend \
 --category=$category --division=$division  --quiet \
---skip_submission_generation=yes --execution-mode=valid $power'
+--skip_submission_generation=yes --execution_mode=valid $power'
 
 submission_cmd_scenario='mlcr generate-run-cmds,inference,_submission  --scenario=$scenario \
 --model=$model --implementation=$implementation --device=$device --backend=$backend \
 --category=$category --division=$division  --quiet  \
---skip_submission_generation=yes --execution-mode=valid $power'
+--skip_submission_generation=yes --execution_mode=valid $power'
 
 
 # run "$MLC_RUN_CMD"
@@ -57,5 +58,6 @@ submission_cmd_scenario='mlcr generate-run-cmds,inference,_submission  --scenari
 #run_test "tf" "6000" "reference" "cpu" "$find_performance_cmd --rerun"
 
 run_test "onnxruntime" "100" "reference" "cpu" "$submission_cmd"
+
 run_test "tf" "100" "reference" "cpu" "$submission_cmd"
 
