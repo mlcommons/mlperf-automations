ac6f327a445bdd0404b4f2ed39f44167419aa8be
sid9993 <sidharthra9993@gmail.com>
2025-02-20 09:31:48 +0530

    Made changes in module.py

--- automation/script/module.py
+++ automation/script/module.py
@@ -5072,7 +5072,7 @@ def any_enable_or_skip_script(meta, env):
             meta_key = [str(v).lower() for v in meta[key]]
 
             if set(meta_key) & set(["yes", "on", "true", "1"]):
-                if is_false(value):
+                if not is_false(value):
                     found = True
             elif set(meta_key) & set(["no", "off", "false", "0", ""]):
                 if value in ["no", "off", "false", "0", ""]:
