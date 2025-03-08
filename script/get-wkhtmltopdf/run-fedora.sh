9c74b11d77d2a31bbb19c18bc90b1c8df5c18962
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-03-09 02:24:07 +0530

    Fix wkhtmltopdf install on RHEL

--- script/get-wkhtmltopdf/run-fedora.sh
+++ script/get-wkhtmltopdf/run-fedora.sh
@@ -6,3 +6,6 @@ wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmlt
 
 # Install the package
 ${MLC_SUDO} yum localinstall -y wkhtmltox-0.12.6-1.centos6.x86_64.rpm --nobest --skip-broken
+
+wkhtmltopdf --version
+test $? -eq 0 || exit $?
