54a7de7309734504fd886c5d9dd0bcf44e4dedd9
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-03-29 01:34:21 +0530

    Fix test script github action

--- .github/scripts/list_modified_files.py
+++ .github/scripts/list_modified_files.py
@@ -2,8 +2,7 @@ import yaml
 import sys
 import json
 
-
-def get_num_runs(filepath):
+def get_file_info(filepath):
     with open(filepath, 'r') as file:
         content = yaml.safe_load(file)
         tests = content.get('tests', {})
@@ -14,23 +13,21 @@ def get_num_runs(filepath):
         uid = content['uid']
         return uid, num_tests
 
-
 def process_files(files):
     filenames = files.split()
     return [
         {
             "file": file,
             "uid": uid,
-            "num_tests": num_tests
+            "num_run": i
         }
         for file in filenames
-        if file.endswith('meta.yaml') and (uid := get_num_runs(file))
-        for _, num_tests in [uid]
+        if file.endswith('meta.yaml') and (uid, num_tests := get_file_info(file))
+        for i in range(1, num_tests + 1)
     ]
-
 
 if __name__ == "__main__":
     changed_files = sys.stdin.read().strip()
     processed_files = process_files(changed_files)
     json_processed_files = json.dumps(processed_files)
-    print(f"::set-output name=modified_files::{json_processed_files}")
+    print(f"::set-output name=processed_files::{json_processed_files}")
