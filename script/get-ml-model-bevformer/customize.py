3f139eb23f514590b6379e57f00acb1399d7632b
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-05-13 21:13:32 +0530

    add automotive 0.5 model download scripts

---
+++ script/get-ml-model-bevformer/customize.py
@@ -0,0 +1,23 @@
+from mlc import utils
+import os
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    if os_info['platform'] == "windows":
+        return {'return': 1, 'error': 'Script not supported in windows yet!'}
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+
+    env['MLC_ML_MODEL_BEVFORMER_PATH'] = os.path.join(env['MLC_ML_MODEL_BEVFORMER_PATH'], env['MLC_ML_MODEL_FILENAME'])
+
+    return {'return': 0}
---
+++ script/get-ml-model-ssd-resnet50/customize.py
@@ -0,0 +1,25 @@
+from mlc import utils
+import os
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    if os_info['platform'] == "windows":
+        return {'return': 1, 'error': 'Script not supported in windows yet!'}
+
+    env['MLC_TMP_REQUIRE_DOWNLOAD'] = "yes"
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+
+    env['MLC_ML_MODEL_SSD_PATH'] = os.path.join(env['MLC_ML_MODEL_SSD_PATH'], env['MLC_ML_MODEL_FILENAME'])
+
+    return {'return': 0}
