8323fb856e2f5490fc0f5678e1a277e94f7412df
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-05-14 21:03:44 +0530

    fix syntax

--- script/get-dataset-cognata-mlcommons/run.sh
+++ script/get-dataset-cognata-mlcommons/run.sh
@@ -7,7 +7,7 @@
 
 #${MLC_PYTHON_BIN_WITH_PATH} contains the path to python binary if "get,python" is added as a dependency
 
-if [[ "$MLC_DOWNLOAD_MODE" != "dry" && "$MLC_TMP_REQUIRE_DOWNLOAD" == "true" && "$MLC_COGNATA_DATASET_TYPE" == "release"]]; then
+if [[ "$MLC_DOWNLOAD_MODE" != "dry" && "$MLC_TMP_REQUIRE_DOWNLOAD" == "true" && "$MLC_COGNATA_DATASET_TYPE" == "release" ]]; then
   cd "${MLC_DATASET_MLCOMMONS_COGNATA_PATH}" || exit
   for f in *.tar.gz; do 
     tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
