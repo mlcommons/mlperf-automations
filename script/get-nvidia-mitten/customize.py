e6beae0598df2ffda620b5a9035b018a44b2531d
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-05-19 01:40:00 +0530

    Upgrade numpy for nvmitten

--- script/get-nvidia-mitten/customize.py
+++ script/get-nvidia-mitten/customize.py
@@ -6,12 +6,11 @@ def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
-    script_path = i['artifact'].path
+    script_path = env['MLC_TMP_CURRENT_SCRIPT_PATH']
 
-    if env.get('MLC_MLPERF_INFERENCE_VERSION', '') == "5.0":
-        extra_run_cmd = 'patch -p1 < {}'.format(os.path.join(
-            script_path, 'patch', 'numpy-mitten-v5.0.patch'))
-        env['EXTRA_RUN_CMD'] = extra_run_cmd
+    extra_run_cmd = 'patch -p1 < {}'.format(os.path.join(
+        script_path, 'patch', 'numpy-mitten-v5.0.patch'))
+    env['EXTRA_RUN_CMD'] = extra_run_cmd
 
     return {'return': 0}
 
