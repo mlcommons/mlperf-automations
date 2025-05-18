a2759a8438162f61cc810718a1c112c1051a1690
Arjun Suresh <arjunsuresh1987@gmail.com>
2025-05-19 01:59:17 +0530

    Fix nvidia-mitten install

--- script/get-nvidia-mitten/customize.py
+++ script/get-nvidia-mitten/customize.py
@@ -6,11 +6,6 @@ def preprocess(i):
 
     os_info = i['os_info']
     env = i['env']
-    script_path = env['MLC_TMP_CURRENT_SCRIPT_PATH']
-
-    extra_run_cmd = 'patch -p1 < {}'.format(os.path.join(
-        script_path, 'patch', 'numpy-mitten-v5.0.patch'))
-    env['EXTRA_RUN_CMD'] = extra_run_cmd
 
     return {'return': 0}
 
