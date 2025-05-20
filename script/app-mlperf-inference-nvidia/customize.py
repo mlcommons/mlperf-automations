796c363439f3551c383db08aa6155548039fdec6
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-05-20 13:02:41 +0530

    update batch size extraction logic

--- script/app-mlperf-inference-nvidia/customize.py
+++ script/app-mlperf-inference-nvidia/customize.py
@@ -519,17 +519,20 @@ def preprocess(i):
         if dla_inference_streams:
             run_config += f" --dla_inference_streams={dla_inference_streams}"
 
-        gpu_batch_size = state.get('batch_size', env.get(
-            'MLC_MLPERF_NVIDIA_HARNESS_GPU_BATCH_SIZE'))
+        if env.get('MLC_MLPERF_INFERENCE_VERSION', '') == "5.0":
+            gpu_batch_size = state.get('batch_size', env.get('MLC_MLPERF_NVIDIA_HARNESS_GPU_BATCH_SIZE'))
+            dla_batch_size = state.get('dla_batch_size', env.get('MLC_MLPERF_NVIDIA_HARNESS_DLA_BATCH_SIZE'))
+        else:
+            gpu_batch_size = env.get('MLC_MLPERF_NVIDIA_HARNESS_GPU_BATCH_SIZE')
+            dla_batch_size = env.get('MLC_MLPERF_NVIDIA_HARNESS_DLA_BATCH_SIZE')
+            
         if gpu_batch_size:
             if env.get('MLC_MLPERF_INFERENCE_VERSION', '') == "5.0":
                 gpu_batch_size = ",".join(
                     f"{key}:{value}" for key,
                     value in gpu_batch_size.items())
             run_config += f" --gpu_batch_size={gpu_batch_size}"
-
-        dla_batch_size = state.get('dla_batch_size', env.get(
-            'MLC_MLPERF_NVIDIA_HARNESS_DLA_BATCH_SIZE'))
+            
         if dla_batch_size:
             if env.get('MLC_MLPERF_INFERENCE_VERSION', '') == "5.0":
                 dla_batch_size = ",".join(
