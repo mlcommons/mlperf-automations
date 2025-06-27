cbb10ded3f7160dba44c9ff25b6238c899b1f0f1
anandhu-eng <anandhusooraj011@gmail.com>
2025-06-28 00:41:36 +0530

    fix folder issue

--- script/get-dataset-whisper/customize.py
+++ script/get-dataset-whisper/customize.py
@@ -20,13 +20,18 @@ def preprocess(i):
     else:
         cwd = env.get('MLC_OUTDIRNAME', os.getcwd())
         data_dir = os.path.join(cwd, 'data')
-        env['MLC_TMP_DATA_DIR'] = data_dir
-        librispeech_dir = os.path.join(cwd, 'LibriSpeech')
-        env['MLC_TMP_LIBRISPEECH_DIR'] = librispeech_dir
+        librispeech_dir = os.path.join(data_dir, 'LibriSpeech')
         utils_dir = os.path.join(
             env['MLC_MLPERF_INFERENCE_SOURCE'],
             'speech2text',
             'utils')
+        
+        # create directories if not exist
+        os.makedirs(data_dir, exist_ok=True)
+        os.makedirs(librispeech_dir, exist_ok=True)
+        
+        env['MLC_TMP_DATA_DIR'] = data_dir
+        env['MLC_TMP_LIBRISPEECH_DIR'] = librispeech_dir
         env['MLC_TMP_UTILS_DIR'] = utils_dir
         env['MLC_DATASET_WHISPER_PATH'] = data_dir
 
