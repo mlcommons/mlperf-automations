4d965d89186e1234b87998842f25c101e9bb786a
anandhu-eng <anandhusooraj011@gmail.com>
2025-06-27 16:15:29 +0530

    add whisper model download script

---
+++ script/get-ml-model-whisper/customize.py
@@ -0,0 +1,27 @@
+from mlc import utils
+from utils import is_true
+import os
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    if os_info['platform'] == "windows":
+        return {'return': 1, 'error': 'Script not supported in windows yet!'}
+
+    if env.get('MLC_ML_MODEL_WHISPER_PATH', '') == '':
+        env['MLC_TMP_REQUIRE_DOWNLOAD'] = "yes"
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+
+    env['MLC_ML_MODEL_FILE_WITH_PATH'] = env['MLC_ML_MODEL_WHISPER_PATH']
+
+    return {'return': 0}
