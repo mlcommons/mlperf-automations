e5748594a40fd63d0b32627a67ac4a1c831ba735
anandhu-eng <anandhusooraj011@gmail.com>
2025-06-30 11:03:50 +0530

    improve log

--- script/install-vllm-from-src/run.sh
+++ script/install-vllm-from-src/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd ${MLC_VLLM_SRC_REPO_PATH}
+echo "vLLM source repo path: ${MLC_VLLM_SRC_REPO_PATH}"
 
-${MLC_RUN_CMD}
+echo '${MLC_RUN_CMD}'
+'${MLC_RUN_CMD}'
 test $? -eq 0 || exit $?
 
