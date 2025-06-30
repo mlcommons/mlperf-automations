2d6c2dd9bb272bb29c0e4659027b5749b696179e
anandhu-eng <anandhusooraj011@gmail.com>
2025-06-30 11:11:56 +0530

    fix cmd construction

--- script/install-vllm-from-src/run.sh
+++ script/install-vllm-from-src/run.sh
@@ -3,7 +3,7 @@
 cd ${MLC_VLLM_SRC_REPO_PATH}
 echo "vLLM source repo path: ${MLC_VLLM_SRC_REPO_PATH}"
 
-echo '${MLC_RUN_CMD}'
-'${MLC_RUN_CMD}'
+echo ${MLC_RUN_CMD}
+${MLC_RUN_CMD}
 test $? -eq 0 || exit $?
 
