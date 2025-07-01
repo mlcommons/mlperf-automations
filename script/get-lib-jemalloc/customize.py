789ed1899944468eda6d636d2ed35877649055d7
Arjun <arjunsuresh1987@gmail.com>
2025-07-01 19:13:51 +0530

    Support more configurations for lib-jemalloc

--- script/get-lib-jemalloc/customize.py
+++ script/get-lib-jemalloc/customize.py
@@ -10,6 +10,16 @@ def preprocess(i):
 
     os_info = i['os_info']
 
+    configure_command = f"""{os.path.join(env['MLC_JEMALLOC_SRC_PATH'], 'configure')} --enable-autogen"""
+    if env.get('MLC_JEMALLOC_LG_QUANTUM', '') != '':
+        configure_command += f""" --with-lg-quantum={env['MLC_JEMALLOC_LG_QUANTUM']} """
+    if env.get('MLC_JEMALLOC_LG_PAGE', '') != '':
+        configure_command += f""" --with-lg-page={env['MLC_JEMALLOC_LG_PAGE']} """
+    if env.get('MLC_JEMALLOC_CONFIG', '') != '':
+        configure_command += f""" {env['MLC_JEMALLOC_CONFIG']} """
+    
+    env['MLC_JEMALLOC_CONFIGURE_COMMAND'] = configure_command
+
     return {'return': 0}
 
 
