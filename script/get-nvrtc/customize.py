1896be7ebf5051a17e074587ded19c92b704edee
anandhu-eng <anandhusooraj011@gmail.com>
2025-09-22 20:42:26 +0530

    Add nvrtc dependency

---
+++ script/get-nvrtc/customize.py
@@ -0,0 +1,20 @@
+from mlc import utils
+import os
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    env = i['env']
+
+    recursion_spaces = i['recursion_spaces']
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+
+    return {'return': 0, 'version': version}
---
+++ script/get-nvrtc/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/${MLC_HOST_PLATFORM_FLAVOR}/cuda-keyring_1.1-1_all.deb
+test $? -eq 0 || exit 1
+
+dpkg -i cuda-keyring_1.1-1_all.deb
+test $? -eq 0 || exit 1
+
+rm cuda-keyring_1.1-1_all.deb
