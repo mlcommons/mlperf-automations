6feb53b788aeddd88579dfc69e1549a28bcaf9d2
anandhu-eng <anandhusooraj011@gmail.com>
2025-09-23 00:00:52 +0530

    remove the nvrtc library before installing new

--- script/get-nvrtc/run.sh
+++ script/get-nvrtc/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [[ $(apt list --installed | grep cuda-nvrtc-dev) ]]; then
+    apt-get remove --purge -y --allow-change-held-packages cuda-nvrtc-dev*
+fi
+
 curl -fsSLO https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/${MLC_HOST_PLATFORM_FLAVOR}/cuda-keyring_1.1-1_all.deb
 test $? -eq 0 || exit 1
 
