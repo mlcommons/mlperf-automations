b12ad9730f067f956aa352c3a5f2a4d62af6ae34
anandhu-eng <anandhusooraj011@gmail.com>
2025-10-07 18:31:04 +0530

    fix extraction paths for r2-downloader

--- script/get-preprocessed-dataset-nuscenes/run.sh
+++ script/get-preprocessed-dataset-nuscenes/run.sh
@@ -13,16 +13,13 @@ if [[ "$MLC_DOWNLOAD_MODE" != "dry" && "$MLC_TMP_REQUIRE_DOWNLOAD" = "yes" && "$
 fi
 
 if [[ "$MLC_DOWNLOAD_MODE" != "dry" && "$MLC_TMP_REQUIRE_DOWNLOAD" = "yes" && "$MLC_DOWNLOAD_TOOL" = "r2-downloader" ]]; then
-  echo "${MLC_PREPROCESSED_DATASET_NUSCENES_PATH}"
-  echo "${MLC_PREPROCESSED_DATASET_NUSCENES_ACC_CHECKER_MIN_FILES_PATH}"
-  exit 1
-  # cd "${MLC_PREPROCESSED_DATASET_NUSCENES_PATH}" || exit
-  # for f in *.tar.gz; do 
-  #   tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
-  # done
-  # cd "${MLC_PREPROCESSED_DATASET_NUSCENES_ACC_CHECKER_MIN_FILES_PATH}" || exit
-  # for f in *.tar.gz; do 
-  #   tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
-  # done
-  # cd - || exit
+  cd "${MLC_PREPROCESSED_DATASET_NUSCENES_PATH}/preprocessed" || exit
+  for f in *.tar.gz; do 
+    tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
+  done
+  cd "${MLC_PREPROCESSED_DATASET_NUSCENES_PATH}" || exit
+  for f in *.tar.gz; do 
+    tar -xzvf "$f" || { echo "Failed to extract $f"; exit 1; }
+  done
+  cd - || exit
 fi
