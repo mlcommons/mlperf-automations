ea967764cf92d28c51d54971fdb1452cba113cd4
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-10-25 11:00:26 +0530

    Refactor run.sh to simplify execution flow
    Removed unnecessary directory creation and file copy for NVIDIA system.

--- script/add-custom-nvidia-system/run.sh
+++ script/add-custom-nvidia-system/run.sh
@@ -5,7 +5,4 @@ if [ "${MLC_CUSTOM_CONFIG}" != "yes" ]; then
     "${MLC_PYTHON_BIN_WITH_PATH}" scripts/custom_systems/add_custom_system.py
     test $? -eq 0 || exit $?
     cd "$CUR"
-else
-    mkdir -p "${MLC_MLPERF_INFERENCE_NVIDIA_CODE_PATH}/configs/${MLC_NVIDIA_SYSTEM_NAME}"
-    cp "${MLC_TMP_CURRENT_SCRIPT_PATH}/dummy_config.py" "${MLC_MLPERF_INFERENCE_NVIDIA_CODE_PATH}/configs/${MLC_NVIDIA_SYSTEM_NAME}/"
 fi
