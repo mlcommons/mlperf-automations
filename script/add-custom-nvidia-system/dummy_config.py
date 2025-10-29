2a823814904032f9d54ea78694f5e9f6d926e480
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2025-10-29 16:29:28 +0530

    Add llm_gen_config_path to workload settings

--- script/add-custom-nvidia-system/dummy_config.py
+++ script/add-custom-nvidia-system/dummy_config.py
@@ -6,8 +6,9 @@ import code.fields.harness as harness_fields
 
 EXPORTS = {
     C.WorkloadSetting(C.HarnessType.Custom, C.AccuracyTarget(0.99), C.PowerSetting.MaxP): {
-
+        llm_fields.llm_gen_config_path: 'code/llama2-70b/tensorrt/generation_config.json',
     },
     C.WorkloadSetting(C.HarnessType.Custom, C.AccuracyTarget(0.999), C.PowerSetting.MaxP): {
+        llm_fields.llm_gen_config_path: 'code/llama2-70b/tensorrt/generation_config.json',
     },
 }
