a0bef75dae8b5d84a3843ca90b3906465712eb51
Arjun <arjunsuresh1987@gmail.com>
2026-02-01 12:13:00 +0000

    Make remote_run winows compatible

--- script/remote-run-commands/customize.py
+++ script/remote-run-commands/customize.py
@@ -1,6 +1,7 @@
 from mlc import utils
 import os
 import subprocess
+import platform
 
 
 def preprocess(i):
@@ -10,11 +11,16 @@ def preprocess(i):
     env = i['env']
 
     cmd_string = ''
+    
+    is_windows = os_info['platform'] == 'windows'
 
-    # pre_run_cmds = env.get('MLC_SSH_PRE_RUN_CMDS', ['source $HOME/cm/bin/activate'])
+    # pre_run_cmds = env.get('MLC_SSH_PRE_RUN_CMDS', ['source $HOME/mlcflow/bin/activate'])
     pre_run_cmds = env.get('MLC_SSH_PRE_RUN_CMDS', [])
 
     files_to_copy = env.get('MLC_SSH_FILES_TO_COPY', [])
+
+    if "<<<HOME>>>" in env.get('MLC_SSH_KEY_FILE', ''):
+        env['MLC_SSH_KEY_FILE'] = env['MLC_SSH_KEY_FILE'].replace("<<<HOME>>>", os.path.expanduser("~"))
 
     run_cmds = env.get('MLC_SSH_RUN_COMMANDS', [])
 
@@ -26,8 +32,11 @@ def preprocess(i):
             cmd = cmd.replace(";;", ",")
             run_cmds[i] = cmd
 
+    # Use semicolon for Unix-like systems, the remote server will handle it
     cmd_string += " ; ".join(run_cmds)
-    user = env.get('MLC_SSH_USER', os.environ.get('USER'))
+    
+    # Get username - on Windows, USERNAME is the env var, not USER
+    user = env.get('MLC_SSH_USER', os.environ.get('USER') or os.environ.get('USERNAME'))
     password = env.get('MLC_SSH_PASSWORD', None)
     host = env.get('MLC_SSH_HOST')
     port = env.get('MLC_SSH_PORT', '22')
@@ -40,8 +49,10 @@ def preprocess(i):
     ssh_cmd = ["ssh", "-p", port]
 
     if env.get("MLC_SSH_SKIP_HOST_VERIFY"):
+        # Use NUL on Windows, /dev/null on Unix
+        null_device = "NUL" if is_windows else "/dev/null"
         ssh_cmd += ["-o", "StrictHostKeyChecking=no",
-                    "-o", "UserKnownHostsFile=/dev/null"]
+                    "-o", f"UserKnownHostsFile={null_device}"]
 
     key_file = env.get("MLC_SSH_KEY_FILE")
     if key_file:
@@ -49,20 +60,34 @@ def preprocess(i):
 
     ssh_cmd_str = " ".join(ssh_cmd)
 
+    # Use double quotes on Windows, single quotes on Unix for better compatibility
+    quote_char = '"' if is_windows else "'"
     ssh_run_command = ssh_cmd_str + " " + user + "@" + host + \
-        password_string + " '" + cmd_string + "'"
+        password_string + " " + quote_char + cmd_string + quote_char
     env['MLC_SSH_CMD'] = ssh_run_command
 
-    # ---- Use sshpass if password is provided ----
+    # ---- Use sshpass if password is provided (only on Unix-like systems) ----
     rsync_base = ["rsync", "-avz"]
 
-    if password:
+    if password and not is_windows:
         rsync_base = ["sshpass", "-p", password] + rsync_base
 
-    target_directory = env['MLC_SSH_TARGET_COPY_DIRECTORY']
+    target_directory = env.get('MLC_SSH_TARGET_COPY_DIRECTORY', '')
 
     # ---- Execute copy commands ----
     for file in files_to_copy:
+        # Check if rsync is available
+        rsync_available = True
+        try:
+            subprocess.run(["rsync", "--version"], capture_output=True, check=True)
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            rsync_available = False
+            
+        if not rsync_available:
+            print(f"⚠️  rsync not found. Skipping file copy for {file}")
+            print("   On Windows, install rsync via WSL, Cygwin, or use Git Bash")
+            continue
+        
         cmd = [
             "rsync",
             "-avz",
