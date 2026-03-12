685043be59abb33d383eca46c3b20da3054da565
anandhu-eng <anandhusooraj011@gmail.com>
2026-03-12 12:02:45 +0530

    support installing mmdetection from src

---
+++ script/install-mmdetection-from-src/customize.py
@@ -0,0 +1,29 @@
+from mlc import utils
+import os
+from utils import is_true
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+
+    if os_info['platform'] == 'windows':
+        return {'return': 1, 'error': 'Windows is not supported in this script yet'}
+
+    env = i['env']
+
+    if env.get('MLC_MMDET_TASK', '') == "automotive":
+        env['MLC_MMDET_TARGET_DIR'] = env['MLC_GIT_CHECKOUT_PATH']
+        pip_version = env.get('MLC_PIP_VERSION', '').strip().split('.')
+        if pip_version and len(pip_version) > 1 and int(pip_version[0]) >= 23:
+            env['MLC_PYTHON_PIP_COMMON_EXTRA'] = " --break-system-packages"
+        print(pip_version)
+        run_cmd = f"pip3 install -v -e . {env.get('MLC_PYTHON_PIP_COMMON_EXTRA', '')}"
+
+    automation = i['automation']
+
+    recursion_spaces = i['recursion_spaces']
+
+    env['MLC_RUN_CMD'] = run_cmd
+
+    return {'return': 0}
