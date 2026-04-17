226ddce9377cde6cf07b31527dc817bcaced3b20
Arjun <arsuresh@amd.com>
2026-04-17 22:58:51 +0530

    Fixes aocl_utils path for lib-aocl-libm

--- script/get-lib-aocl-libm/run.sh
+++ script/get-lib-aocl-libm/run.sh
@@ -4,6 +4,5 @@ if [[ -z ${MLC_GIT_REPO_CHECKOUT_PATH} ]]; then
     exit 1
 fi
 cd ${MLC_GIT_REPO_CHECKOUT_PATH}
-scons -j${MLC_HOST_CPU_TOTAL_PHYSICAL_CORES}
+scons -j${MLC_HOST_CPU_TOTAL_PHYSICAL_CORES} --aocl_utils_install_path=${MLC_AOCL_UTILS_INSTALL_PATH}
 test $? -eq 0 || exit $?
-
