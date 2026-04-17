1154a628ad3a805e996b4ad42f6180df937b0ee0
Arjun <arsuresh@amd.com>
2026-04-18 00:14:44 +0530

    Fixed symlinks for amdlibm

--- script/get-lib-aocl-libm/run.sh
+++ script/get-lib-aocl-libm/run.sh
@@ -13,9 +13,11 @@ cd ${MLC_GIT_REPO_CHECKOUT_PATH}
 scons -j${MLC_HOST_CPU_TOTAL_PHYSICAL_CORES} --aocl_utils_install_path=${MLC_AOCL_UTILS_INSTALL_PATH}
 test $? -eq 0 || exit $?
 
-# Create compatibility symlinks
+# Copy fast libs from subdirectory and create compatibility symlinks
 LIB_DIR="${MLC_GIT_REPO_CHECKOUT_PATH}/build/aocl-release/src"
 cd "${LIB_DIR}"
+cp -f fast/libalmfast.so . 2>/dev/null || true
+cp -f fast/libalmfast.a . 2>/dev/null || true
 ln -sf libalm.so libamdlibm.so
 ln -sf libalm.a libamdlibm.a
 ln -sf libalmfast.so libamdlibmfast.so
