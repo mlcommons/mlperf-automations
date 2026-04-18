d967ee2f23e58dfb45e282f8bdf57c13757ee329
Arjun <arsuresh@amd.com>
2026-04-18 06:53:47 +0530

    Fix GH action failures

--- .github/scripts/list_modified_scripts_with_secrets.py
+++ .github/scripts/list_modified_scripts_with_secrets.py
@@ -3,6 +3,8 @@ import sys
 import json
 import os
 
+TRIGGER_FILES = ['meta.yaml', 'customize.py', 'run.sh']
+
 
 def get_file_info(filepath):
     with open(filepath, 'r') as file:
@@ -17,29 +19,45 @@ def get_file_info(filepath):
         return uid, num_tests
 
 
-def process_files(files):
+def get_trigger_directories(filenames):
+    """Return unique directories that contain a changed trigger file (meta.yaml, customize.py, run.sh)."""
+    dirs = set()
+    for file in filenames:
+        if os.path.basename(file) in TRIGGER_FILES:
+            dirs.add(os.path.dirname(file))
+    return dirs
+
+
+def get_meta_paths(files):
+    """Return deduplicated meta.yaml paths for directories with any trigger file changed."""
     filenames = files.split(",")
+    dirs = get_trigger_directories(filenames)
+    return [os.path.join(d, 'meta.yaml') for d in dirs if os.path.exists(os.path.join(d, 'meta.yaml'))]
+
+
+def process_files(files):
+    meta_paths = get_meta_paths(files)
     return [
         {
-            "file": file,
+            "file": meta,
             "uid": uid,
             "num_run": i
         }
-        for file in filenames if os.path.basename(file) == 'meta.yaml'
-        for uid, num_tests in [get_file_info(file)]
+        for meta in meta_paths
+        for uid, num_tests in [get_file_info(meta)]
         for i in range(1, num_tests + 1)
     ]
 
 
 def get_modified_metas(files):
-    filenames = files.split(",")
+    meta_paths = get_meta_paths(files)
     return [
         {
-            "file": file,
+            "file": meta,
             "uid": uid,
         }
-        for file in filenames if os.path.basename(file) == 'meta.yaml'
-        for uid, num_tests in [get_file_info(file)]
+        for meta in meta_paths
+        for uid, num_tests in [get_file_info(meta)]
     ]
 
 
