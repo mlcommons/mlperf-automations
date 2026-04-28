7ba5cfb80a8a2cf1909344d6e75cc1238186da27
arjunsuresh <arjunsuresh1987@gmail.com>
2026-04-28 15:25:16 +0530

    Fix phoronix customize.py: use env.get for test name instead of undefined variable

--- script/benchmark-program-phoronix/customize.py
+++ script/benchmark-program-phoronix/customize.py
@@ -17,7 +17,7 @@ def preprocess(i):
     result_id = env.get('MLC_PHORONIX_RESULT_IDENTIFIER', 'mlc-benchmark')
     env['MLC_PHORONIX_RESULT_IDENTIFIER'] = result_id
 
-    logger.info(f"Phoronix test: {test}, result ID: {result_id}")
+    logger.info(f"Phoronix test: {env.get('MLC_PHORONIX_TEST', '')}, result ID: {result_id}")
 
     return {'return': 0}
 
