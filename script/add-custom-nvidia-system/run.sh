0b1d0bff24099eebbd2a419bf9751861c343088a
Arjun Suresh <arjunsuresh1987@gmail.com>
2026-05-09 01:00:09 +0530

    Fixes for Nvidia mlperf inference v6.0

--- script/add-custom-nvidia-system/run.sh
+++ script/add-custom-nvidia-system/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 CUR=$PWD
 cd ${MLC_MLPERF_INFERENCE_NVIDIA_CODE_PATH}
-${MLC_PYTHON_BIN_WITH_PATH} scripts/custom_systems/add_custom_system.py
-test $? -eq 0 || exit $?
+if [[ -f scripts/custom_systems/add_custom_system.py ]]; then
+  ${MLC_PYTHON_BIN_WITH_PATH} scripts/custom_systems/add_custom_system.py
+  test $? -eq 0 || exit $?
+else
+  echo "add_custom_system.py not found (v6.0+ uses SYSTEM_NAME env var instead), skipping..."
+fi
