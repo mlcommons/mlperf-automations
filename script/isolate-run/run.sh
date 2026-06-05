ea02bdb3d106adbbc898f2d33f0141a24f09669b
arjunsuresh <arjunsuresh1987@gmail.com>
2026-06-05 17:49:32 +0530

    Added is_true for isolate-run script

--- script/isolate-run/run.sh
+++ script/isolate-run/run.sh
@@ -6,11 +6,16 @@ function exit_if_error() {
   test $? -eq 0 || exit $?
 }
 
+function is_true() {
+  local val="${1,,}"  # lowercase
+  [[ "${val}" == "yes" || "${val}" == "true" || "${val}" == "1" || "${val}" == "on" ]]
+}
+
 function run() {
   echo "Running: "
   echo "$1"
   echo ""
-  if [[ ${MLC_FAKE_RUN} != 'yes' ]]; then
+  if ! is_true "${MLC_FAKE_RUN}"; then
     eval "$1"
     exit_if_error
   fi
@@ -21,12 +26,12 @@ CURRENT_USER=$(whoami)
 ACTION=${MLC_ISOLATE_ACTION}
 
 # --- No new logins ---
-if [[ "${MLC_ISOLATE_NO_NEW_LOGINS}" == "yes" ]]; then
+if is_true "${MLC_ISOLATE_NO_NEW_LOGINS}"; then
   if [[ "${ACTION}" == "set" ]]; then
     run "${SUDO} touch /var/run/nologin"
     echo "Created /var/run/nologin - new non-root logins blocked"
   elif [[ "${ACTION}" == "unset" ]]; then
-    if [[ "${MLC_ISOLATE_NOLOGIN_EXISTED}" == "yes" ]]; then
+    if is_true "${MLC_ISOLATE_NOLOGIN_EXISTED}"; then
       echo "/var/run/nologin existed before set - leaving it in place"
     else
       run "${SUDO} rm -f /var/run/nologin"
@@ -36,9 +41,9 @@ if [[ "${MLC_ISOLATE_NO_NEW_LOGINS}" == "yes" ]]; then
 fi
 
 # --- Force logout other users ---
-if [[ "${MLC_ISOLATE_FORCE_LOGOUT}" == "yes" ]]; then
+if is_true "${MLC_ISOLATE_FORCE_LOGOUT}"; then
   if [[ "${ACTION}" == "set" ]]; then
-    echo "Forcing logout of all sessions except current user (${CURRENT_USER})..."
+    echo "Forcing logout of all sessions except current user (${CURRENT_USER}).."
     OTHER_USERS=$(who | awk -v me="${CURRENT_USER}" '$1 != me {print $1}' | sort -u)
     if [[ -n "${OTHER_USERS}" ]]; then
       for u in ${OTHER_USERS}; do
@@ -54,7 +59,7 @@ if [[ "${MLC_ISOLATE_FORCE_LOGOUT}" == "yes" ]]; then
 fi
 
 # --- Network isolation ---
-if [[ "${MLC_ISOLATE_NETWORK}" == "yes" ]]; then
+if is_true "${MLC_ISOLATE_NETWORK}"; then
   if [[ "${ACTION}" == "set" ]]; then
     echo "Disabling non-loopback network interfaces..."
     IFS=',' read -ra IFACES <<< "${MLC_ISOLATE_SAVED_INTERFACES}"
