f10574f296317fa7158a144c6ef120707e4dca0b
mlc-automations <3246381+mlc-automations@users.noreply.github.com>
2026-09-10 17:40:52 +0000

    [Automated Commit] Format Codebase

--- script/test-profiler-uprof/customize.py
+++ script/test-profiler-uprof/customize.py
@@ -5,7 +5,8 @@ import os
 def preprocess(i):
     env = i['env']
     if env.get('MLC_UPROF_BIN_WITH_PATH', '') == '':
-        return {'return': 1, 'error': 'MLC_UPROF_BIN_WITH_PATH not set; get,profiler,uprof must run first'}
+        return {
+            'return': 1, 'error': 'MLC_UPROF_BIN_WITH_PATH not set; get,profiler,uprof must run first'}
     return {'return': 0}
 
 
